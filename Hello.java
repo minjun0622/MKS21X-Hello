f4c911e2f5aaf45ba72a7b05da5b99cf495966e1
mseo00 <mseo00@stuy.edu>
2018-11-27 09:48:50 -0500

    New computer shenanigans.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello my name is Minjun Seo, this was pretty hard to do.");
+        System.out.println("Hello my name is Minjun Seo, this was pretty hard to do. Wrong haha now I am in a room with touchscreen computers.");
+  
     }
 }
